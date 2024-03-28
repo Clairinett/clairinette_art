d157324ba0cfa81a778e6620b8f892eea4161b8d
[Clairinette] <[claire.crinier@gmail.com]>
2024-03-28 19:23:07 +0100

    home controller OK header OK

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,16 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+
+const nav = document.getElementById('offcanvasNavbar');
+const li = document.querySelectorAll('li'); 
+
+if (window.matchMedia("(min-width:1200px)").matches) {
+    nav.classList.add('show'); //la nav reste ouverte grâce à l'ajout de la class show
+    
+    li.forEach(function (allLi) { //selectionne tout les li grace au foreach qui parcours le tableau (nodelist) de querySelectorAll (un tableau avec tout les li)
+        allLi.removeAttribute("data-bs-dismiss"); //enlève l'attribut data-bs-dismiss qui en mobile sert à ferme la nav quand on clique sur un lien
+    });
+};
